63d04529d9a12590ebe327a160b426b952c9729e
Sanhe Hu <husanhe@gmail.com>
2019-11-20 00:23:58 -0500

    update requirement deploy script

--- devops/deploy.py
+++ devops/deploy.py
@@ -4,6 +4,7 @@ from seedinvest_monitor.cf import template, config
 from seedinvest_monitor.boto_ses import boto_ses
 from troposphere_mate import StackManager
 
+template.to_file("master.json")
 stack_manager = StackManager(
     boto_ses=boto_ses,
     cft_bucket=config.S3_BUCKET_FOR_DEPLOY.get_value(),
