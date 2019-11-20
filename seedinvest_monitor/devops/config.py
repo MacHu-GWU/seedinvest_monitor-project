6ce77a078757db1bcdb1e4d34cd69c2912953810
Sanhe Hu <husanhe@gmail.com>
2019-11-20 00:24:19 -0500

    update config

--- seedinvest_monitor/devops/config.py
+++ seedinvest_monitor/devops/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from pathlib_mate import Path
 from configirl import ConfigClass, Constant, Derivable
+from pathlib_mate import Path
 
 
 class Config(ConfigClass):
@@ -24,7 +24,7 @@ class Config(ConfigClass):
     def get_ENVIRONMENT_NAME(self):
         return "{}-{}".format(self.PROJECT_NAME_SLUG.get_value(self), self.STAGE.get_value())
 
-    AWS_PROFILE = Constant()
+    AWS_PROFILE_FOR_DEPLOY = Constant()
     AWS_PROFILE_FOR_PYTHON = Derivable()
 
     @AWS_PROFILE_FOR_PYTHON.getter
@@ -32,7 +32,10 @@ class Config(ConfigClass):
         if self.is_aws_lambda_runtime():
             return None
         else:
-            return self.AWS_PROFILE.get_value()
+            return self.AWS_PROFILE_FOR_DEPLOY.get_value()
+
+    AWS_ACCOUNT_ID = Constant()
+    AWS_REGION = Constant()
 
     S3_BUCKET_FOR_DEPLOY = Constant()
 
@@ -42,5 +45,30 @@ class Config(ConfigClass):
     def get_DYNAMODB_TABLE_STARTUP(self):
         return "{}-startup".format(self.ENVIRONMENT_NAME.get_value())
 
+    SQS_QUEUE_URL = Derivable()
 
+    @SQS_QUEUE_URL.getter
+    def get_SQS_QUEUE_URL(self):
+        return f"https://sqs.{self.AWS_REGION.get_value()}.amazonaws.com/{self.AWS_ACCOUNT_ID.get_value()}/{self.ENVIRONMENT_NAME.get_value()}-download-job"
 
+    LAMBDA_CODE_S3_KEY = Derivable()
+
+    @LAMBDA_CODE_S3_KEY.getter
+    def get_LAMBDA_CODE_S3_KEY(self):
+        from seedinvest_monitor._version import __version__
+        return f"lambda/MacHu-GWU/seedinvest_monitor-project/{__version__}/source.zip"
+
+    LAMBDA_LAYER_VERSION = Constant()
+    LAMBDA_LAYERS = Derivable()
+
+    @LAMBDA_LAYERS.getter
+    def get_LAMBDA_LAYERS(self):
+        return [
+            f"arn:aws:lambda:us-east-1:110330507156:layer:seedinvest_monitor:{self.LAMBDA_LAYER_VERSION.get_value()}"
+        ]
+
+    UPDATE_NEW_PROJECT_RATE = Constant()
+
+    HTML_UPDATE_INTERVAL_IN_SECONDS = Constant()
+
+    START_CRAWLER_INTERVAL_IN_SECONDS = Constant()
--- seedinvest_monitor/devops/config_init.py
+++ seedinvest_monitor/devops/config_init.py
@@ -6,7 +6,7 @@ from seedinvest_monitor.devops.config import Config
 
 config = Config()
 if config.is_aws_lambda_runtime():
-    pass
+    config.update_from_env_var(prefix="SEEDINVEST_MONITOR_")
 else:
     config.update(json.load(Path(config.CONFIG_DIR, "00-config-shared.json").abspath, verbose=False))
     config.update(json.load(Path(config.CONFIG_DIR, "01-config-dev.json").abspath, verbose=False))
