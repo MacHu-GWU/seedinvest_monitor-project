1906c9cbdc565c04914817e973067b96efaf57d4
Sanhe Hu <husanhe@gmail.com>
2019-11-20 00:24:47 -0500

    update model

--- seedinvest_monitor/model.py
+++ seedinvest_monitor/model.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import gzip
 import os
-from attrs_mate import attr, AttrsClass
-from troposphere_mate import NOTHING
-from pynamodb.models import Model
+
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, BinaryAttribute, MapAttribute
+from pynamodb.models import Model
+
 from seedinvest_monitor.devops.config_init import config
+
+try:
+    from .crawler import url_builder
+except:
+    from seedinvest_monitor.crawler import url_builder
 
 if not config.is_aws_lambda_runtime():
     os.environ["AWS_DEFAULT_PROFILE"] = config.AWS_PROFILE_FOR_PYTHON.get_value()
@@ -36,10 +42,18 @@ class Startup(Model):
 
     id = UnicodeAttribute(hash_key=True)
     project_create_at = UnicodeAttribute(null=True)
-    raw_html = BinaryAttribute(null=True)
+    compressed_raw_html = BinaryAttribute(null=True)
     html_download_at = UnicodeAttribute(null=True)
     details = MapAttribute(null=True)
     details_update_at = UnicodeAttribute(null=True)
+
+    @property
+    def url(self):
+        return url_builder.url_project(self.id)
+
+    @property
+    def raw_html(self):
+        return gzip.decompress(self.compressed_raw_html).decode("utf-8")
 
 
 class Event(Model):
@@ -53,14 +67,17 @@ class Event(Model):
 if __name__ == "__main__":
     res = Event.query(hash_key="a1")
     print(list(res))
+    #
+    # res = Event.scan(Event.time > "2019-03-01")
+    # print(list(res))
+    #
+    # event = Event(id="a1", value=3)
+    # event.save()
+    # try:
+    #     event = Event.get("a2")
+    # except Event.DoesNotExist:
+    #     print(1)
+    # event.update(actions=[Event.value.set(3)])
 
-    res = Event.scan(Event.time > "2019-03-01")
-    print(list(res))
-
-    event = Event(id="a1", value=3)
-    event.save()
-    try:
-        event = Event.get("a2")
-    except Event.DoesNotExist:
-        print(1)
-    event.update(actions=[Event.value.set(3)])
+    startup = Startup.get("hitch")
+    print(startup.raw_html)
